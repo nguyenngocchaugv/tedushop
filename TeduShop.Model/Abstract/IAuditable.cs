7e8235205fd771a7a7a9291f5bf6c22632b907b0
nguyen ngoc chau <nguyenngocchaugv@yahoo.com.vn>
2016-05-16 23:36:15 +0700

    model

---
+++ TeduShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Models
+{
+    public interface IAuditable
+    {
+        DateTime? CreatedDate { set; get; }
+        string CreateBy { set; get; }
+        DateTime? UpdatedDate { set; get; }
+        string UpdateBy { set; get; }
+
+        string MetaKeyword { set; get; }
+        string MetaDescription { set; get; }
+
+        bool Status { set; get; }
+    }
+}
