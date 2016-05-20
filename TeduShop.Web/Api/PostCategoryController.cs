8a2923f7fddabaa550c50ddfc5f6b45ff1bb3c53
nguyen ngoc chau <nguyenngocchaugv@yahoo.com.vn>
2016-05-20 11:51:30 +0700

    Edit Dependency Injection autofac for web

--- TeduShop.Web/Api/PostCategoryController.cs
+++ TeduShop.Web/Api/PostCategoryController.cs
@@ -25,20 +25,10 @@ namespace TeduShop.Web.Api
         public HttpResponseMessage Get(HttpRequestMessage request)
         {
             return CreateHttpResponse(request, () =>
-            {
-                HttpResponseMessage response = null;
-                if (ModelState.IsValid)
-                {
-                    request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
-                }
-                else
-                {
+            {               
                     var listCategory = _postCategoryService.GetAll();
-
-                    response = request.CreateResponse(HttpStatusCode.OK, listCategory);
-
-                }
-                return response;
+                    HttpResponseMessage response = request.CreateResponse(HttpStatusCode.OK, listCategory);
+                    return response;
             });
         }
         public HttpResponseMessage Post(HttpRequestMessage request,PostCategory postCategory)
