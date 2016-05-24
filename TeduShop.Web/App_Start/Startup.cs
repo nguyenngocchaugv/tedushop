e4f22cc92482d7409c5b4952610dc5a075f45175
nguyen ngoc chau <nguyenngocchaugv@yahoo.com.vn>
2016-05-24 15:39:36 +0700

    add identity

--- TeduShop.Web/App_Start/Startup.cs
+++ TeduShop.Web/App_Start/Startup.cs
@@ -4,25 +4,31 @@ using Microsoft.Owin;
 using Owin;
 using Autofac;
 using System.Reflection;
-using Autofac.Integration.Mvc;
 using TeduShop.Data.Infrastructure;
-using TeduShop.Data;
 using TeduShop.Data.Repositories;
 using TeduShop.Service;
 using System.Web.Mvc;
 using System.Web.Http;
+using Autofac.Integration.Mvc;
 using Autofac.Integration.WebApi;
+using TeduShop.Data;
+using Microsoft.AspNet.Identity;
+using TeduShop.Model.Models;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataProtection;
+using System.Web;
 
 [assembly: OwinStartup(typeof(TeduShop.Web.App_Start.Startup))]
 
 namespace TeduShop.Web.App_Start
 {
-    public class Startup
+    public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
             ConfigAutofac(app);
+            ConfigureAuth(app);
         }
 
         public void ConfigAutofac(IAppBuilder app)
@@ -37,6 +43,13 @@ namespace TeduShop.Web.App_Start
             builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
 
             builder.RegisterType<TeduShopDbContext>().AsSelf().InstancePerRequest();
+
+            //Asp.net Identity
+            builder.RegisterType<ApplicationUserStore>().As<IUserStore<ApplicationUser>>().InstancePerRequest();
+            builder.RegisterType<TeduShop.Web.App_Start.ApplicationUserStore.ApplicationUserManager>().AsSelf().InstancePerRequest();
+            builder.RegisterType<TeduShop.Web.App_Start.ApplicationUserStore.ApplicationSignInManager>().AsSelf().InstancePerRequest();
+            builder.Register(c => HttpContext.Current.GetOwinContext().Authentication).InstancePerRequest();
+            builder.Register(c => app.GetDataProtectionProvider()).InstancePerRequest();
 
             //Repositories
             builder.RegisterAssemblyTypes(typeof(PostCategoryRepository).Assembly)
