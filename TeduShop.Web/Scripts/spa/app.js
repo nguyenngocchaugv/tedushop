9a3615ef3ae7ab492792f1e1f4d3b8bffa6c025f
nguyen ngoc chau <nguyenngocchaugv@yahoo.com.vn>
2016-05-25 15:02:22 +0700

    service angularjs

--- TeduShop.Web/Scripts/spa/app.js
+++ TeduShop.Web/Scripts/spa/app.js
@@ -5,19 +5,40 @@ var myApp = angular.module('myModule', []);
 
 //Creating the controller and registering with the module all done.
 myApp.controller("SchoolController", SchoolController);
-myApp.controller("StudentController", StudentController);
-myApp.controller("TeacherController", TeacherController);
+myApp.service('Validator', Validator);
+//myApp.controller("StudentController", StudentController);
+//myApp.controller("TeacherController", TeacherController);
 
-//myApp.$inject = ['$scope'];
-//declare
-function SchoolController($scope) {
-    $scope.message = "This is my message from School";
+////myApp.$inject = ['$scope'];
+////declare
+
+
+//function StudentController($scope) {
+//    $scope.message = "This is my message from Student ";
+//}
+
+//function TeacherController($scope) {
+//    $scope.message = "This is my message from Teacher";
+//}
+
+SchoolController.$inject = ['$scope', 'Validator'];
+
+function SchoolController($scope, Validator) {
+    $scope.checkNumber = function () {
+        $scope.message = Validator.checkNumber($scope.num);
+    }
+    $scope.num = 1;
 }
 
-function StudentController($scope) {
-    $scope.message = "This is my message from Student ";
-}
-
-function TeacherController($scope) {
-    $scope.message = "This is my message from Teacher";
+function Validator($window) {
+    return {
+        checkNumber: checkNumber
+    }
+    function checkNumber(input) {
+        if (input % 2 == 0) {
+            return 'This is even';
+        }
+        else
+            return 'This is odd';
+    }
 }
